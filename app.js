44c21109852a6c4f7950c986778c07f4cec8e154
iamcelestino <trcelestino488@gmail.com>
2023-10-04 11:56:50 -0700

    added error states

--- app.js
+++ app.js
@@ -1,13 +1,47 @@
 
-const arrowCalcuator = document.querySelector("img");
 const form = document.querySelector("form");
 const currentAge = document.querySelector(".age");
+const errorMessage = document.querySelectorAll(".error__message");
+const validErrorMessage = document.querySelectorAll(".valid__error");
+const inputs = document.querySelectorAll("input");
 
 const calculateAge = (dayOfBirth, monthOfBirth, yearOfBirth) => {
-    const years = new Date().getFullYear() - yearOfBirth;
-    const months = new Date().getMonth() - monthOfBirth;
-    const days = (new Date().getDate() - dayOfBirth) - 1;
-    updateUi(days, months, years)
+
+    const years = (new Date().getFullYear() - yearOfBirth);
+    const months = (new Date().getMonth() - monthOfBirth);
+    const days = ((new Date().getDate() - dayOfBirth) - 1) * (-1);
+
+    if ((dayOfBirth > 31) && (monthOfBirth > 12) &&  (yearOfBirth > new Date().getFullYear())){
+        validErrorMessage.forEach(error => {
+            error.classList.remove("valid__error");
+
+            inputs.forEach(item => {
+                item.classList.add("inputError")
+            })
+        })
+    }
+    else if((dayOfBirth > 31)) {
+        validErrorMessage[0].classList.remove("valid__error");
+    } 
+    else if((monthOfBirth > 12)) {
+        validErrorMessage[1].classList.remove("valid__error");
+    }
+    else if((yearOfBirth  > new Date().getFullYear())) {
+        
+        validErrorMessage[2].classList.remove("valid__error");
+    }
+    else if((dayOfBirth === "") || (monthOfBirth === "") || (yearOfBirth === "")) {
+        errorMessage.forEach(error => {
+            error.classList.remove("error__message");
+            
+            inputs.forEach(item => {
+                item.classList.add("inputError")
+            })
+        })
+    } 
+    else {
+        updateUi(days, months, years);
+    }
 }
 
 const updateUi = (day, month, year) => {
@@ -19,13 +53,20 @@ const updateUi = (day, month, year) => {
     currentAge.innerHTML = age;
 }
 
-form.addEventListener("keyup", event => {
+function getUserBirthday(event) {
     event.preventDefault();
-    const dayOfBirth = form.day.value;
-    const monthOfBirth = form.month.value;
-    const yearOfBirth = form.year.value;
+    const dayOfBirth = form.day.value.trim();
+    const monthOfBirth = form.month.value.trim();
+    const yearOfBirth = form.year.value.trim();
     calculateAge(dayOfBirth, monthOfBirth, yearOfBirth);
-})
+   
+}
+
+form.addEventListener("submit", getUserBirthday);
+
+
+
+
 
 
 
